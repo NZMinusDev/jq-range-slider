85799cb7b6c05c172cb62a264fe4e3887b19d6e4
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-15 09:43:29 +0300

    feat(TypingHelper: add custom utils)

--- app/src/utils/devTools/tools/TypingHelper.ts
+++ app/src/utils/devTools/tools/TypingHelper.ts
@@ -1,27 +1,17 @@
+export type GenericFunc<TFuncArgs extends unknown[], TFuncReturn extends unknown> = (
+  ...funcArgs: TFuncArgs
+) => TFuncReturn;
 /**
- * typeof on steroids reads [Symbol.toStringTag] for more controlled result of comparison
- * @param obj - any value
- * @returns type
  * @example
- * console.log(TypeOf(123)) // Number
- * console.log(TypeOf(null)) // Null
- * console.log(TypeOf(undefined)) // Undefined
- * console.log(TypeOf([])) // Array
- * console.log(TypeOf(alert)) // Function
- * console.log(TypeOf(window)) // Window
- * console.log(new XMLHttpRequest()) // XMLHttpRequest
- *
- * let user = {};
- * console.log(user) // Object
- * user = { [Symbol.toStringTag]: "User" };
- * console.log(user) // User
+ * class Class {
+ *  constructor(a:number,b?:string) {}
+ * }
+ * type T = GenericConstructor<typeof Class>
+ * // type T = new (a: number, b?: string | undefined) => typeof Class
  */
-export default function TypeOf(obj: unknown): string {
-  const type = Object.prototype.toString.call(obj);
-  return type.slice(type.indexOf(" ") + 1, type.indexOf("]"));
-}
-
-export type GenericFunc<FA extends unknown, FR extends unknown> = (...funcArgs: FA[]) => FR;
+export type GenericConstructor<TCreator extends new (...args: any[]) => any> = new (
+  ...args: ConstructorParameters<TCreator>
+) => TCreator;
 
 /**
  * @example
@@ -38,13 +28,13 @@ export type GenericFunc<FA extends unknown, FR extends unknown> = (...funcArgs: 
  * type T5 = Unpacked<Unpacked<Promise<string>[]>>;
  * //   ^ = type T5 = string
  */
-export type Unpacked<T> = T extends (infer U)[]
-  ? U
-  : T extends (...args: any[]) => infer U
-  ? U
-  : T extends Promise<infer U>
-  ? U
-  : T;
+export type Unpacked<TType> = TType extends (infer TUnpacked)[]
+  ? TUnpacked
+  : TType extends (...args: any[]) => infer TUnpacked
+  ? TUnpacked
+  : TType extends Promise<infer TUnpacked>
+  ? TUnpacked
+  : TType;
 /**
  * @example
  * type test = {
@@ -64,7 +54,7 @@ export type Unpacked<T> = T extends (infer U)[]
  *   param5: [{ p1: "p1", p2: 0 }],
  * };
  */
-export type ArrayPacked<T> = { [K in keyof T]: Array<T[K]> };
+export type ArrayPacked<TObject extends object> = { [TKey in keyof TObject]: Array<TObject[TKey]> };
 
 /**
  * @example
@@ -90,14 +80,132 @@ export type ArrayPacked<T> = { [K in keyof T]: Array<T[K]> };
  * //       subparts: Part[];
  * //   }
  */
-export type FunctionPropertyNames<T> = {
-  [K in keyof T]: T[K] extends Function ? K : never;
-}[keyof T];
-export type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
-export type NonFunctionPropertyNames<T> = {
-  [K in keyof T]: T[K] extends Function ? never : K;
-}[keyof T];
-export type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+export type FunctionPropertyNames<TObject extends object> = {
+  [TKey in keyof TObject]: TObject[TKey] extends Function ? TKey : never;
+}[keyof TObject];
+export type FunctionProperties<TObject extends object> = Pick<
+  TObject,
+  FunctionPropertyNames<TObject>
+>;
+export type NonFunctionPropertyNames<TObject extends object> = {
+  [TKey in keyof TObject]: TObject[TKey] extends Function ? never : TKey;
+}[keyof TObject];
+export type NonFunctionProperties<TObject extends object> = Pick<
+  TObject,
+  NonFunctionPropertyNames<TObject>
+>;
+
+/**
+ * @example
+ * interface SomeType {
+ *   required: string;
+ *   optional?: number;
+ *   requiredButPossiblyUndefined: boolean | undefined;
+ *   [k: string]: unknown; // index signature
+ * }
+ * type SomeTypeRequiredKeys = RequiredKeys<SomeType>;
+ * // type SomeTypeRequiredKeys = "required" | "requiredButPossiblyUndefined" 🙂
+ *
+ * type SomeTypeOptionalKeys = OptionalKeys<SomeType>;
+ * // type SomeTypeOptionalKeys = "optional" 🙂
+ *
+ * type SomeTypeIndexKeys = IndexKeyType<SomeType>;
+ * // type SomeTypeIndexKeys = string 🙂
+ */
+export type RequiredKeys<TObject extends object> = {
+  [TKey in keyof TObject]-?: string extends TKey
+    ? never
+    : number extends TKey
+    ? never
+    : {} extends Pick<TObject, TKey>
+    ? never
+    : TKey;
+} extends { [_ in keyof TObject]-?: infer TInfer }
+  ? TInfer extends keyof TObject
+    ? TInfer
+    : never
+  : never;
+export type OptionalKeys<TObject extends object> = {
+  [TKey in keyof TObject]-?: string extends TKey
+    ? never
+    : number extends TKey
+    ? never
+    : {} extends Pick<TObject, TKey>
+    ? TKey
+    : never;
+} extends { [_ in keyof TObject]-?: infer TInfer }
+  ? TInfer extends keyof TObject
+    ? TInfer
+    : never
+  : never;
+export type IndexKeyType<TObject> = string extends keyof TObject
+  ? string
+  : number extends keyof TObject
+  ? number
+  : never;
+
+/**
+ * @example
+ * type MyTuple = [container:HTMLElement, id: number, name?:string, data?:{}]
+ *
+ * type MyTupleWithRequired = RequiredTupleValues<MyTuple>
+ * // type MyTupleWithRequired = [HTMLElement, number]
+ * type MyTupleWithOptional = OptionalTupleValues<MyTuple>
+ * // type MyTupleWithRequired = [name?: string | undefined, data?: {} | undefined]
+ */
+export type RequiredTupleValues<TTuple extends unknown[]> = FilterType<
+  RequiredToNeverInTuple<TTuple>,
+  never
+>;
+export type OptionalTupleValues<TTuple extends unknown[]> = FilterType<
+  OptionalToNeverInTuple<TTuple>,
+  null
+>;
+type RequiredToNeverInTuple<TTuple extends unknown[]> = {
+  [TKey in keyof TTuple]: TTuple[TKey] extends TTuple[RequiredKeys<TTuple>] ? TTuple[TKey] : never;
+};
+type OptionalToNeverInTuple<TTuple extends unknown[]> = {
+  [TKey in keyof TTuple]: RequiredKeys<TTuple> extends never
+    ? TTuple[TKey]
+    : TTuple[TKey] extends TTuple[RequiredKeys<TTuple>]
+    ? null
+    : TTuple[TKey];
+};
+
+/**
+ * @example
+ * type T1 = FilterType<[number, string, undefined, number],undefined>
+ * // [number, string, number]
+ * type T2 = FilterType<[1, undefined, 2],undefined> // [1, 2]
+ * type T3 = FilterType<[undefined, 2],undefined> // [2]
+ * type T4 = FilterType<[2, undefined],undefined> // [2]
+ * type T5 = FilterType<[undefined, undefined, 2],undefined> // [2]
+ * type T6 = FilterType<[undefined],undefined> // []
+ * type T7 = FilterType<[],undefined> // []
+ */
+export type FilterType<TTuple extends unknown[], TType> = TTuple extends []
+  ? []
+  : TTuple extends [infer H, ...infer R]
+  ? H extends TType
+    ? FilterType<R, TType>
+    : [H, ...FilterType<R, TType>]
+  : TTuple;
+
+/**
+ * @example
+ * interface SomeType {
+ *   required: string;
+ *   optional?: number;
+ *   requiredButPossiblyUndefined: boolean | undefined;
+ *   [k: string]: unknown; // index signature
+ * }
+ * type T =  RequiredValues<SomeType>
+ * // { required: string; requiredButPossiblyUndefined: boolean | undefined; }
+ * type T2 =  OptionalValues<SomeType>
+ * // { optional?: number | undefined; }
+ */
+export type RequiredValues<TObject extends object> = Pick<TObject, RequiredKeys<TObject>>;
+export type OptionalValues<TObject extends object> = Pick<TObject, OptionalKeys<TObject>>;
 
 /**
  * @example
@@ -107,7 +215,7 @@ export type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
  * people.next.next.name;
  * people.next.next.next.name;
  */
-export type LinkedList<Type> = Type & { next: LinkedList<Type> };
+export type LinkedList<TType> = TType & { next: LinkedList<TType> };
 /**
  * @example
  * type T1 = Boxed<string>;
@@ -121,6 +229,6 @@ export type LinkedList<Type> = Type & { next: LinkedList<Type> };
  * type T3 = Boxed<string | number[]>;
  * //   ^ = type T3 = BoxedValue | BoxedArray
  */
-export type Boxed<T> = T extends any[] ? BoxedArray<T[number]> : BoxedValue<T>;
-type BoxedValue<T> = { value: T };
-type BoxedArray<T> = { array: T[] };
+export type Boxed<TType> = TType extends any[] ? BoxedArray<TType[number]> : BoxedValue<TType>;
+type BoxedValue<TType> = { value: TType };
+type BoxedArray<TType> = { array: TType[] };
