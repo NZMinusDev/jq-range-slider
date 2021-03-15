2a9fc675d68c09c4f8642247733cf84096389e07
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-15 10:00:17 +0300

    feat(pips.view.test: add opt-s,init,get,set tests)

--- app/src/components/common.blocks/primitives/range-slider/__pips/view/range-slider__pips.view.test.ts
+++ app/src/components/common.blocks/primitives/range-slider/__pips/view/range-slider__pips.view.test.ts
@@ -1,32 +1,101 @@
-import RangeSliderPipsView, { DEFAULT_OPTIONS, PipsOptions } from "./range-slider__pips.view";
+import RangeSliderPipsView, { DEFAULT_OPTIONS } from "./range-slider__pips.view";
 
-import { testInit } from "@utils/devTools/tools/UnitTestingHelper";
+import {
+  InstancePropsExpecter,
+  testInit,
+  testInitDEFAULT_OPTIONS,
+  testGetter,
+  testSetter,
+} from "@utils/devTools/tools/UnitTestingHelper";
+import { collapsingParseInt } from "@utils/devTools/tools/ParserHelper";
 
-testInit(RangeSliderPipsView, DEFAULT_OPTIONS, {
-  invalidOptions: [
-    { amount: -2, density: 5.5543 },
-    { amount: 5.5543, density: -2 },
-  ],
-  partialOptions: [{ mode: "count", amount: 3 }],
-  fullOptions: [
+const viewPropertiesExpecter: InstancePropsExpecter<
+  ConstructorParameters<typeof RangeSliderPipsView>,
+  RangeSliderPipsView
+> = function ({ instance }) {
+  expect(instance["_options"].amount).toBeGreaterThanOrEqual(2);
+  expect(instance["_options"].amount).toEqual(collapsingParseInt(`${instance["_options"].amount}`));
+
+  expect(instance["_options"].density).toBeGreaterThanOrEqual(0);
+  expect(instance["_options"].density).toEqual(
+    collapsingParseInt(`${instance["_options"].density}`)
+  );
+
+  const formatterMock = jest.fn(instance["_options"].formatter);
+  formatterMock(1);
+  expect(formatterMock).toHaveReturned();
+};
+
+testInitDEFAULT_OPTIONS(
+  RangeSliderPipsView,
+  [document.createElement("div"), DEFAULT_OPTIONS],
+  viewPropertiesExpecter
+);
+
+testInit({
+  Creator: RangeSliderPipsView,
+  differentConstructorArgs: {
+    validRequiredArguments: [[document.createElement("div")]],
+    invalidOptionalArguments: [[{ amount: -2, density: 5.5543 }], [{ amount: 4.89, density: -5 }]],
+    partialOptionalArguments: [[{ mode: "count", amount: 3 }]],
+    fullOptionalArguments: [
+      [
+        {
+          mode: "count",
+          amount: 4,
+          density: 6,
+          formatter: (value: number) => `${value.toString()}$`,
+        },
+      ],
+    ],
+  },
+  instancePropsExpecter: viewPropertiesExpecter,
+  propsToSet: new Map().set("dom.self", 0).set("_options", 1),
+});
+
+testGetter({
+  Creator: RangeSliderPipsView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderPipsView.prototype.getOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    returns: "_options",
+  },
+});
+testSetter({
+  Creator: RangeSliderPipsView,
+  constructorArgs: [
+    document.createElement("div"),
     {
       mode: "count",
-      amount: 4,
-      density: 6,
-      formatter: {
-        to: (value: number) => `${value.toString()}$`,
-        from: (value: string) => +parseFloat(value).toFixed(4),
-      },
+      amount: 5,
+      density: 3,
+      formatter: (value: number) => `${value.toString()}%$`,
     },
   ],
-  expectValidProperties({ viewOptions, passedOptions }) {
-    expect(viewOptions.amount).toBeGreaterThanOrEqual(2);
-    expect(viewOptions.amount).toEqual(parseInt(`${viewOptions.amount}`));
-
-    expect(viewOptions.density).toBeGreaterThanOrEqual(0);
-    expect(viewOptions.density).toEqual(parseInt(`${viewOptions.density}`));
-
-    expect(viewOptions.formatter).not.toBe(passedOptions.formatter);
-    expect(viewOptions.formatter).toStrictEqual(passedOptions.formatter);
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderPipsView.prototype.setOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    differentArguments: {
+      invalidOptionalArguments: [
+        [{ amount: -2, density: 5.5543 }],
+        [{ amount: 4.89, density: -5 }],
+      ],
+      partialOptionalArguments: [[{ amount: 3, density: 1 }]],
+      fullOptionalArguments: [
+        [
+          {
+            mode: "count",
+            amount: 4,
+            density: 6,
+            formatter: (value: number) => `${value.toString()}$`,
+          },
+        ],
+      ],
+    },
   },
+  propsToSet: new Map().set("_options", 0),
+  resetPropsTo: new Map().set("_options", DEFAULT_OPTIONS),
 });
