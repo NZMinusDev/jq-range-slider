e817169a76c47cb629e2c4cd2f1412ddf32f59ef
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-15 09:58:29 +0300

    feat(UnitTestingHelper: add testSetter)

--- app/src/utils/devTools/tools/UnitTestingHelper.ts
+++ app/src/utils/devTools/tools/UnitTestingHelper.ts
@@ -105,6 +105,95 @@ export function testGetter<
     methodOfInstanceToTest,
   });
 }
+/**
+ * Testing(expect non undefined bounded(by instancePropsExpecter) properties of instance. Also expects the method sets targeted property, resets property to default if undefined is passed and returns this. Expects passed arguments are immutable and deep cloned).
+ * @param Creator A Class or Function
+ * @param constructorArgs arguments to pass to constructor, should be full(with optional arguments) if you use resetPropsTo with the same default options as in constructor for reset testing purpose
+ * @param instancePropsExpecter callback with expect calls for instance state
+ * @param methodOfInstanceToTest data about tested method
+ * @param propsToSet association of valid method's args with properties of instance, for example: new Map().set("_options", 0).set("taxes.3", 1).set("payout","2.doc.payout") - It means the first arg set this._options property and the second one set this.taxes[3] property, the third one set this.payout as doc.payout property of 2 argument
+ * @param resetPropsTo association of properties of instance with reset to values, for example: new Map().set("_options", DEFAULT_OPTIONS) - It means this._options should be DEFAULT_OPTIONS after resetting
+ */
+export function testSetter<
+  TCreator extends new (...args: TCreatorArgs) => TInstance,
+  TCreatorArgs extends unknown[],
+  TMethod extends (args: any) => any,
+  TInstance extends object
+>({
+  Creator,
+  constructorArgs,
+  instancePropsExpecter,
+  methodOfInstanceToTest,
+  propsToSet,
+  resetPropsTo,
+}: {
+  Creator: TCreator;
+  constructorArgs: TCreatorArgs;
+  instancePropsExpecter: InstancePropsExpecter<TCreatorArgs, TInstance>;
+  methodOfInstanceToTest: MethodOfInstanceToTest<TMethod, Parameters<TMethod>, TInstance>;
+  propsToSet: PropsToSet<number | string>;
+  resetPropsTo: ResetPropsTo<unknown>;
+}) {
+  const argsToReset = methodOfInstanceToTest.differentArguments
+    ? methodOfInstanceToTest.differentArguments.validRequiredArguments
+      ? methodOfInstanceToTest.differentArguments.validRequiredArguments[0]
+      : []
+    : [];
+
+  function defaultSetterExpecterDecorator(
+    expecter: InstanceMethodExpecter<Parameters<TMethod>, TInstance>
+  ): InstanceMethodExpecter<Parameters<TMethod>, TInstance> {
+    return function ({ mock, passedArgs, instance }) {
+      expect(mock).toHaveReturnedWith(instance);
+
+      //reset when undefined is passed
+      if (Object.is(passedArgs, argsToReset)) {
+        resetPropsTo.forEach((defaultValue, pathToPropInInstance) => {
+          expect(instance[pathToPropInInstance]).toEqual(defaultValue);
+        });
+      }
+
+      expecter({ mock, passedArgs, instance });
+    };
+  }
+
+  methodOfInstanceToTest.expecter = defaultSetterExpecterDecorator(methodOfInstanceToTest.expecter);
+  runMethodOfInstanceWithDifferentArguments({
+    Creator,
+    differentConstructorArgs: { validRequiredArguments: [constructorArgs] } as DifferentArguments<
+      TCreatorArgs
+    >,
+    instancePropsExpecter: instancePropsExpecter as InstancePropsExpecter<
+      TCreatorArgs | Parameters<TMethod>,
+      TInstance
+    >,
+    methodOfInstanceToTest,
+    propsToSet,
+  });
+
+  //reset when undefined is passed
+  const methodOfInstanceToRun = {
+    methodReference: methodOfInstanceToTest.methodReference,
+    argsToPass: (argsToReset as unknown) as Parameters<TMethod>,
+    expecter: methodOfInstanceToTest.expecter,
+  } as MethodOfInstanceToRun<TMethod, Parameters<TMethod>, TInstance>;
+  describe(`${
+    typeof methodOfInstanceToTest.methodReference === "string"
+      ? methodOfInstanceToTest.methodReference
+      : methodOfInstanceToTest.methodReference.name
+  }`, () => {
+    testInstance({
+      describeSentence: "reset (undefined is passed)",
+      Creator,
+      constructorArgs,
+      instancePropsExpecter: instancePropsExpecter as InstancePropsExpecter<
+        TCreatorArgs | Parameters<TMethod>,
+        TInstance
+      >,
+      methodOfInstanceToRun,
+    });
+  });
+}
 
 export type InstancePropsExpecter<TArgs extends unknown[], TInstance> = (parts: {
   passedArgs: TArgs;
@@ -344,6 +433,7 @@ type MethodOfInstanceToRun<
 type PathToPropInInstance = string;
 type PropToGet = PathToPropInInstance;
 type PropsToSet<TArgValue> = Map<PathToPropInInstance, TArgValue>;
+type ResetPropsTo<TDefaultValue> = Map<PathToPropInInstance, TDefaultValue>;
 
 function expectValidInstanceProps<
   TInstance,
