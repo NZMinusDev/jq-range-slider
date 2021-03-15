179bf85319a88b002094d25de547320f9585cd43
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-15 09:47:46 +0300

    feat(FunctionsConvertingHelper: add cloneFunction)

--- app/src/utils/devTools/tools/FunctionsConvertingHelper.ts
+++ app/src/utils/devTools/tools/FunctionsConvertingHelper.ts
@@ -1,3 +1,5 @@
+import { GenericFunc } from "./TypingHelper";
+
 /**
  * Currying of function
  * @param func
@@ -12,7 +14,7 @@
  *alert( curriedSum(1)(2,3) ); // 6
  *alert( curriedSum(1)(2)(3) ); // 6
  */
-export default function curry(func: (...args: unknown[]) => unknown) {
+export function curry(func: (...args: unknown[]) => unknown) {
   return function curried(...args: unknown[]) {
     if (args.length >= func.length) {
       return func.apply(this, args);
@@ -24,4 +26,34 @@ export default function curry(func: (...args: unknown[]) => unknown) {
   };
 }
 
+/**
+ * Clone function
+ * @param func function to clone
+ * @returns clone of function
+ * @example
+ * const test1 =  function(a,b,c) {
+ *   return a+b+c;
+ * };
+ *
+ * console.log(test1 === clone(test1)); // false
+ * console.log(test1(1,1,1) === clone(test1)(1,1,1)); // true
+ * console.log(clone(test1)(1,1,1)); // 3
+ */
+export function cloneFunction<TFuncArgs extends unknown[], TFuncReturn>(
+  func: GenericFunc<TFuncArgs, TFuncReturn> | any
+) {
+  const that = func;
+  const temp = function () {
+    return that.apply(func, (arguments as unknown) as TFuncArgs);
+  };
 
+  for (let key in func) {
+    if (func.hasOwnProperty(key)) {
+      temp[key] = func[key];
+    }
+  }
+
+  Object.defineProperty(temp, "name", { value: func.name });
+
+  return temp;
+}
