ad9d21b581245ecdf3c5f5e2c61326eccd94941e
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-15 09:49:49 +0300

    feat(ParserHelper: add collapsingParseInt/Float)

--- app/src/utils/devTools/tools/ParserHelper.ts
+++ app/src/utils/devTools/tools/ParserHelper.ts
@@ -4,6 +4,12 @@ export function parseFileExtension(fullPath: string) {
   return { filePath, fileExt };
 }
 
+export function collapsingParseInt(str: string): number {
+  return +parseInt(str.replace(/[^0-9]-./g, "")).toFixed(0);
+}
+export function collapsingParseFloat(str: string, precision = 2): number {
+  return +parseFloat(str.replace(/[^0-9-.]/g, "")).toFixed(precision);
+}
 export function getPrecision(number: number) {
   return number.toString().includes(".") ? number.toString().split(".").pop()!.length : 0;
 }
