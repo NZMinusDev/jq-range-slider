e61e4f83a3bcdcfcab7da808dd4528f4532a021d
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-23 09:12:21 +0300

    feat(add range tests of opt manupulation)

--- app/src/components/common.blocks/primitives/range-slider/__range/view/range-slider__range.view.test.ts
+++ app/src/components/common.blocks/primitives/range-slider/__range/view/range-slider__range.view.test.ts
@@ -1,20 +1,62 @@
-import RangeSliderRangeView, { DEFAULT_OPTIONS, RangeOptions } from "./range-slider__range.view";
+import RangeSliderRangeView, { DEFAULT_OPTIONS } from "./range-slider__range.view";
 
-import { testInit } from "@utils/devTools/tools/UnitTestingHelper";
+import {
+  InstancePropsExpecter,
+  testInit,
+  testInitDEFAULT_OPTIONS,
+  testGetter,
+  testSetter,
+  DifferentArguments,
+} from "@utils/devTools/tools/UnitTestingHelper";
 
-testInit(RangeSliderRangeView, DEFAULT_OPTIONS, {
-  invalidOptions: [{ step: -10 }, { step: Infinity }, { step: 0 }],
-  partialOptions: [{ isConnected: true }],
-  fullOptions: [
-    {
-      isConnected: true,
-      step: 100,
-    },
-  ],
-  expectValidProperties({ viewOptions }) {
-    if (viewOptions.step != "none") {
-      expect(viewOptions.step).toBeGreaterThan(0);
-      expect(viewOptions.step).toBeLessThanOrEqual(Number.MAX_VALUE);
-    }
+const viewPropertiesExpecter: InstancePropsExpecter<
+  ConstructorParameters<typeof RangeSliderRangeView>,
+  RangeSliderRangeView
+> = function ({ instance }) {};
+
+const differentOptionsArg: DifferentArguments<Parameters<
+  typeof RangeSliderRangeView.prototype.setOptions
+>> = {
+  fullOptionalArguments: [[{ isConnected: true }]],
+};
+
+testInitDEFAULT_OPTIONS(
+  RangeSliderRangeView,
+  [document.createElement("div"), DEFAULT_OPTIONS],
+  viewPropertiesExpecter
+);
+
+testInit({
+  Creator: RangeSliderRangeView,
+  differentConstructorArgs: {
+    validRequiredArguments: [[document.createElement("div")]],
+    ...(differentOptionsArg as DifferentArguments<
+      ConstructorParameters<typeof RangeSliderRangeView>
+    >),
   },
+  instancePropsExpecter: viewPropertiesExpecter,
+  propsToSet: new Map().set("dom.self", 0).set("_options", 1),
+});
+
+testGetter({
+  Creator: RangeSliderRangeView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderRangeView.prototype.getOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    returns: "_options",
+  },
+});
+testSetter({
+  Creator: RangeSliderRangeView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderRangeView.prototype.setOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    differentArguments: differentOptionsArg,
+  },
+  propsToSet: new Map().set("_options", 0),
+  resetPropsTo: new Map().set("_options", DEFAULT_OPTIONS),
 });
