ca92b8fb3b71023ab9372e8f3437e137fed9180a
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-23 09:11:30 +0300

    feat(tested pips realization of options manup...)

--- app/src/components/common.blocks/primitives/range-slider/__pips/view/range-slider__pips.view.ts
+++ app/src/components/common.blocks/primitives/range-slider/__pips/view/range-slider__pips.view.ts
@@ -2,10 +2,9 @@ import template from "./range-slider__pips.view.pug";
 import "./range-slider__pips.scss";
 
 import { MVPView } from "@utils/devTools/tools/PluginCreationHelper";
-import defaultsDeep from "lodash-es/defaultsDeep";
 import { getPrecision, collapsingParseInt } from "@utils/devTools/tools/ParserHelper";
 
-export default interface RangeSliderPipsView extends MVPView<PipsOptions> {
+export default interface RangeSliderPipsView {
   getModeOption(): PipsOptions["mode"];
   getAmountOption(): PipsOptions["amount"];
   getDensityOption(): PipsOptions["density"];
@@ -27,30 +26,16 @@ export const DEFAULT_OPTIONS: Required<PipsOptions> = {
   mode: "range",
   amount: 2,
   density: 0,
-  formatter: (value: number) => value.toLocaleString(),
+  formatter: (value: number) => value.toFixed(2).toLocaleString(),
 };
 
-export default class RangeSliderPipsView implements RangeSliderPipsView {
-  readonly dom: { self: HTMLElement };
-
-  private _options: Required<PipsOptions>;
-
+export default class RangeSliderPipsView
+  extends MVPView<Required<PipsOptions>, PipsOptions>
+  implements RangeSliderPipsView {
   constructor(container: HTMLElement, options: PipsOptions = DEFAULT_OPTIONS) {
-    this.dom = { self: container };
-
-    this._options = defaultsDeep({}, options, DEFAULT_OPTIONS);
-
-    this._fixOptions();
+    super(container, DEFAULT_OPTIONS, options);
   }
 
-  getOptions() {
-    return {
-      mode: this.getModeOption(),
-      amount: this.getAmountOption(),
-      density: this.getDensityOption(),
-      formatter: this.getFormatterOption(),
-    } as Required<PipsOptions>;
-  }
   getModeOption() {
     return this._options.mode;
   }
@@ -64,16 +49,6 @@ export default class RangeSliderPipsView implements RangeSliderPipsView {
     return this._options.formatter;
   }
 
-  setOptions(options?: PipsOptions) {
-    if (options === undefined) {
-      this.setModeOption().setAmountOption().setDensityOption().setFormatterOption();
-    } else {
-      this._options = defaultsDeep({}, options, this._options);
-      this._fixOptions();
-    }
-
-    return this;
-  }
   setModeOption(mode: PipsOptions["mode"] = DEFAULT_OPTIONS.mode) {
     this._options.mode = mode;
 
@@ -97,26 +72,23 @@ export default class RangeSliderPipsView implements RangeSliderPipsView {
     return this;
   }
 
-  private _fixOptions() {
-    let fixOptionMethodName;
-    Object.keys(this._options).forEach((option) => {
-      fixOptionMethodName = `_fix${option[0].toUpperCase() + option.slice(1)}Option`;
-      if (this[fixOptionMethodName]) this[fixOptionMethodName]();
-    });
-  }
-  private _fixAmountOption() {
+  protected _fixAmountOption() {
     this._options.amount =
       this._options.amount < DEFAULT_OPTIONS["amount"] || getPrecision(this._options.amount) > 0
         ? DEFAULT_OPTIONS["amount"]
         : this._options.amount;
     this._options.amount = collapsingParseInt(`${this._options.amount}`);
+
+    return this;
   }
-  private _fixDensityOption() {
+  protected _fixDensityOption() {
     this._options.density =
       this._options.density < DEFAULT_OPTIONS["density"] || getPrecision(this._options.density) > 0
         ? DEFAULT_OPTIONS["density"]
         : this._options.density;
     this._options.density = collapsingParseInt(`${this._options.density}`);
+
+    return this;
   }
 }
 
