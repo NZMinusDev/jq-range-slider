da7ef23022b75d8bb96cb93af7c2af559a93539c
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-23 09:13:26 +0300

    feat(add tooltip tests of opt manupulation)

--- app/src/components/common.blocks/primitives/range-slider/__tooltip/view/range-slider__tooltip.view.test.ts
+++ app/src/components/common.blocks/primitives/range-slider/__tooltip/view/range-slider__tooltip.view.test.ts
@@ -1,23 +1,74 @@
-import RangeSliderTooltipView, {
-  DEFAULT_OPTIONS,
-  TooltipOptions,
-} from "./range-slider__tooltip.view";
+import RangeSliderTooltipView, { DEFAULT_OPTIONS } from "./range-slider__tooltip.view";
 
-import { testInit } from "@utils/devTools/tools/UnitTestingHelper";
+import {
+  InstancePropsExpecter,
+  testInit,
+  testInitDEFAULT_OPTIONS,
+  testGetter,
+  testSetter,
+  DifferentArguments,
+} from "@utils/devTools/tools/UnitTestingHelper";
 
-testInit(RangeSliderTooltipView, DEFAULT_OPTIONS, {
-  partialOptions: [{ isHidden: true }],
-  fullOptions: [
-    {
-      formatter: {
-        to: (value: number) => `${value.toString()}$`,
-        from: (value: string) => +parseFloat(value).toFixed(4),
+const viewPropertiesExpecter: InstancePropsExpecter<
+  ConstructorParameters<typeof RangeSliderTooltipView>,
+  RangeSliderTooltipView
+> = function ({ instance }) {
+  const formatterMock = jest.fn(instance["_options"].formatter);
+  formatterMock(12.345);
+  expect(formatterMock).toHaveReturned();
+};
+
+const differentOptionsArg: DifferentArguments<Parameters<
+  typeof RangeSliderTooltipView.prototype.setOptions
+>> = {
+  partialOptionalArguments: [[{ isHidden: true }]],
+  fullOptionalArguments: [
+    [
+      {
+        formatter: (value: number) => `${value.toFixed(0).toLocaleString()}$`,
+        isHidden: true,
       },
-      isHidden: true,
-    },
+    ],
   ],
-  expectValidProperties({ viewOptions }) {
-    expect(jest.fn(viewOptions.formatter?.from)("12.345")).toHaveReturned();
-    expect(jest.fn(viewOptions.formatter?.to)(12.345)).toHaveReturned();
+};
+
+testInitDEFAULT_OPTIONS(
+  RangeSliderTooltipView,
+  [document.createElement("div"), DEFAULT_OPTIONS],
+  viewPropertiesExpecter
+);
+
+testInit({
+  Creator: RangeSliderTooltipView,
+  differentConstructorArgs: {
+    validRequiredArguments: [[document.createElement("div")]],
+    ...(differentOptionsArg as DifferentArguments<
+      ConstructorParameters<typeof RangeSliderTooltipView>
+    >),
   },
+  instancePropsExpecter: viewPropertiesExpecter,
+  propsToSet: new Map().set("dom.self", 0).set("_options", 1),
+});
+
+testGetter({
+  Creator: RangeSliderTooltipView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderTooltipView.prototype.getOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    returns: "_options",
+  },
+});
+testSetter({
+  Creator: RangeSliderTooltipView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderTooltipView.prototype.setOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    differentArguments: differentOptionsArg,
+  },
+  propsToSet: new Map().set("_options", 0),
+  resetPropsTo: new Map().set("_options", DEFAULT_OPTIONS),
 });
