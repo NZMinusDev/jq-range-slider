657c849a6a54cd36e8ad1ff836a1b3f8fc6bb32a
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-23 09:10:18 +0300

    feat(add isEven helper)

--- app/src/utils/devTools/tools/ProcessingOfPrimitiveDataHelper.ts
+++ app/src/utils/devTools/tools/ProcessingOfPrimitiveDataHelper.ts
@@ -1,8 +1,16 @@
 /**
  * Callback function for Array<number>.prototype.sort()
  */
-export function compareNumeric(a: number, b: number) {
+export function ascending(a: number, b: number) {
   return a - b;
+}
+/**
+ * Check even or not the number
+ * @param number any number
+ * @returns true if event false otherwise
+ */
+export function isEven(number: number) {
+  return number % 2 == 0 ? true : false;
 }
 
 /**
@@ -17,6 +25,6 @@ export function compareLocaleString(a: string, b: string) {
 /**
  * Replacement or str.slice() cause of surrogate couples support.
  */
-export function slice(str:string, start:number, end:number) {
-  return Array.from(str).slice(start, end).join('');
+export function slice(str: string, start: number, end: number) {
+  return Array.from(str).slice(start, end).join("");
 }
