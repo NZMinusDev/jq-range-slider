05df46da1a8a409c71993f2a9b860340657f2466
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-23 09:12:57 +0300

    feat(add thumb tests of opt manupulation)

--- app/src/components/common.blocks/primitives/range-slider/__thumb/view/range-slider__thumb.view.test.ts
+++ app/src/components/common.blocks/primitives/range-slider/__thumb/view/range-slider__thumb.view.test.ts
@@ -1,17 +1,88 @@
-import RangeSliderThumbView, { DEFAULT_OPTIONS, ThumbOptions } from "./range-slider__thumb.view";
+import RangeSliderThumbView, { DEFAULT_OPTIONS } from "./range-slider__thumb.view";
 
-import { testInit } from "@utils/devTools/tools/UnitTestingHelper";
+import {
+  InstancePropsExpecter,
+  testInit,
+  testInitDEFAULT_OPTIONS,
+  testGetter,
+  testSetter,
+  DifferentArguments,
+} from "@utils/devTools/tools/UnitTestingHelper";
 
-testInit(RangeSliderThumbView, DEFAULT_OPTIONS, {
-  invalidOptions: [{ start: -10 }],
-  fullOptions: [
-    {
-      start: 5000,
-    },
+const viewPropertiesExpecter: InstancePropsExpecter<
+  ConstructorParameters<typeof RangeSliderThumbView>,
+  RangeSliderThumbView
+> = function ({ instance }) {
+  expect(instance["_id"]).toBeGreaterThanOrEqual(0);
+
+  expect(instance["_options"].start).toBeLessThanOrEqual(Number.MAX_SAFE_INTEGER);
+  expect(instance["_options"].start).toBeGreaterThanOrEqual(Number.MIN_SAFE_INTEGER);
+};
+
+const differentOptionsArg: DifferentArguments<Parameters<
+  typeof RangeSliderThumbView.prototype.setOptions
+>> = {
+  invalidOptionalArguments: [
+    [{ start: Infinity }],
+    [{ start: Number.MAX_SAFE_INTEGER + 1 }],
+    [{ start: Number.MIN_SAFE_INTEGER - 1 }],
   ],
-  expectValidProperties({ viewOptions, view }) {
-    expect((view as any)._id).toBeGreaterThanOrEqual(0);
+  fullOptionalArguments: [
+    [
+      {
+        start: 5000,
+      },
+    ],
+  ],
+};
 
-    expect(viewOptions.start).toBeGreaterThanOrEqual(0);
+testInitDEFAULT_OPTIONS(
+  RangeSliderThumbView,
+  [document.createElement("div"), DEFAULT_OPTIONS],
+  viewPropertiesExpecter
+);
+
+testInit({
+  Creator: RangeSliderThumbView,
+  differentConstructorArgs: {
+    validRequiredArguments: [[document.createElement("div")]],
+    ...(differentOptionsArg as DifferentArguments<
+      ConstructorParameters<typeof RangeSliderThumbView>
+    >),
   },
+  instancePropsExpecter: viewPropertiesExpecter,
+  propsToSet: new Map().set("dom.self", 0).set("_options", 1),
+});
+
+testGetter({
+  Creator: RangeSliderThumbView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderThumbView.prototype.getId,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    returns: "_id",
+  },
+});
+testGetter({
+  Creator: RangeSliderThumbView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderThumbView.prototype.getOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    returns: "_options",
+  },
+});
+testSetter({
+  Creator: RangeSliderThumbView,
+  constructorArgs: [document.createElement("div")],
+  instancePropsExpecter: viewPropertiesExpecter,
+  methodOfInstanceToTest: {
+    methodReference: RangeSliderThumbView.prototype.setOptions,
+    expecter: ({ mock, passedArgs, instance }) => {},
+    differentArguments: differentOptionsArg,
+  },
+  propsToSet: new Map().set("_options", 0),
+  resetPropsTo: new Map().set("_options", DEFAULT_OPTIONS),
 });
