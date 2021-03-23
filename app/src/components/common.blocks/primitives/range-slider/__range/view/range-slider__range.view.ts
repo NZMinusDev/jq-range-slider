c47a7513e2246f8077f7e74d804e62c645c23550
NZMinus <developer.n.zaytsev@gmail.com>
2021-03-23 09:12:38 +0300

    feat(tested range realization of options manup...)

--- app/src/components/common.blocks/primitives/range-slider/__range/view/range-slider__range.view.ts
+++ app/src/components/common.blocks/primitives/range-slider/__range/view/range-slider__range.view.ts
@@ -2,48 +2,34 @@ import template from "./range-slider__range.view.pug";
 import "./range-slider__range.scss";
 
 import { MVPView } from "@utils/devTools/tools/PluginCreationHelper";
-import defaultsDeep from "lodash-es/defaultsDeep";
 
-export default interface RangeSliderRangeView extends MVPView<RangeOptions> {
-  getConnectOption(): RangeOptions["isConnected"];
-  setConnectOption(connect?: RangeOptions["isConnected"]): this;
-  getStepOption(): RangeOptions["step"];
-  setStepOption(step?: RangeOptions["step"]): this;
+export default interface RangeSliderRangeView {
+  getIsConnectedOption(): RangeOptions["isConnected"];
+  setIsConnectedOption(connect?: RangeOptions["isConnected"]): this;
 }
 
 export type RangeOptions = {
   isConnected?: boolean;
-  step?: number | "none";
 };
 
 export const DEFAULT_OPTIONS: Required<RangeOptions> = {
   isConnected: false,
-  step: "none",
 };
 
-export default class RangeSliderRangeView {
-  readonly dom: { self: HTMLElement };
-
-  private _options: Required<RangeOptions>;
-
+export default class RangeSliderRangeView
+  extends MVPView<Required<RangeOptions>, RangeOptions>
+  implements RangeSliderRangeView {
   constructor(container: HTMLElement, options: RangeOptions = DEFAULT_OPTIONS) {
-    this.dom = { self: container };
-    this._options = defaultsDeep(options, DEFAULT_OPTIONS);
-
-    this._fixOptions();
+    super(container, DEFAULT_OPTIONS, options);
   }
 
-  private _fixOptions(): void {
-    let fixOptionMethodName;
-    Object.keys(this._options).forEach((option) => {
-      fixOptionMethodName = `_fix${option[0].toUpperCase() + option.slice(1)}Option`;
-      if (this[fixOptionMethodName]) this[fixOptionMethodName]();
-    });
+  getIsConnectedOption() {
+    return this._options.isConnected;
   }
-  private _fixStepOption() {
-    if (this._options.step !== "none") {
-      if (this._options.step <= 0) this._options.step = 1;
-      if (this._options.step > Number.MAX_VALUE) this._options.step = Number.MAX_VALUE;
-    }
+
+  setIsConnectedOption(isConnected = DEFAULT_OPTIONS.isConnected) {
+    this._options.isConnected = isConnected;
+
+    return this;
   }
 }
